3eab56489632b130c2e5c343a3320e79168b325b
Buhari Bashir <bubashir@nfiu.gov.ng>
2026-03-09 18:28:24 +0100

    ok

--- app/frontend/vite.config.js
+++ app/frontend/vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
     port: 3000,
     proxy: {
       '/api': {
-        target: 'http://localhost:8000',
+        target: 'https://buhari123-trademark-similarity-api.hf.space',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ''),
       },
